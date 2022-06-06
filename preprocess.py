c508c666f0401109fc13b8c0c2bcb7f5a5d853f9
Minh Anh <anhnm@vtc.edu.vn>
2022-06-06 11:28:01 +0700

    Modified cleaners and filelist

--- preprocess.py
+++ preprocess.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument("--out_extension", default="cleaned")
   parser.add_argument("--text_index", default=2, type=int)
-  parser.add_argument("--filelists", nargs="+", default=["../dataset/filelists/vctk_filelist.txt"])
+  parser.add_argument("--filelists", nargs="+", default=["filelists/vctk1.txt"])
   parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
 
   args = parser.parse_args()
@@ -19,10 +19,13 @@ if __name__ == '__main__':
     print("START:", filelist)
     filepaths_and_text = load_filepaths_and_text(filelist)
     for i in tqdm(range(len(filepaths_and_text))):
-      original_text = filepaths_and_text[i][args.text_index]
-      cleaned_text = text._clean_text(original_text, args.text_cleaners)
-      filepaths_and_text[i][args.text_index] = cleaned_text
+        try:
+            original_text = filepaths_and_text[i][args.text_index]
+            cleaned_text = text._clean_text(original_text, args.text_cleaners)
+            filepaths_and_text[i][args.text_index] = cleaned_text
+        except KeyboardInterrupt:
+            pass
 
     new_filelist = filelist + "." + args.out_extension
     with open(new_filelist, "w", encoding="utf-8") as f:
-      f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
+        f.writelines(["|".join(x) + "\n" for x in filepaths_and_text])
